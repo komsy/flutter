09007bad5d60b8a28c65dc595618bd1842cf5e9c
Komsy <koometest@gmail.com>
2024-08-20 15:20:12 +0300

    settings page

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:k_store/features/personalization/views/settings/settings.dart';
 import 'package:k_store/features/shop/screens/home/home.dart';
 import 'package:k_store/utils/constants/colors.dart';
 import 'package:k_store/utils/helpers/helper_functions.dart';
@@ -39,5 +40,6 @@ class NavigationMenu extends StatelessWidget {
 class NavigationController extends GetxController {
   final Rx<int> selectedIndex =0.obs;
 
-  final screens= [const HomeSreen() ,Container(color: Colors.purple),Container(color: Colors.orange),Container(color: Colors.blue),];
+  final screens= [const HomeSreen() ,
+  Container(color: Colors.purple),Container(color: Colors.orange),const SetttingsScreen()];
 }
